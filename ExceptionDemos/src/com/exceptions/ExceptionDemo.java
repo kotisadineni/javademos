5905ac22ff66d83f4ea4cf8c1243fe6cb531026c
kotisadineni <sadineni2107@gmail.com>
2017-05-10 09:33:40 +0530

    adding modifications

--- ExceptionDemos/src/com/exceptions/ExceptionDemo.java
+++ ExceptionDemos/src/com/exceptions/ExceptionDemo.java
@@ -3,8 +3,16 @@ package com.exceptions;
 public class ExceptionDemo {
 
 	public static void main(String[] args) {
+		
+		
+		try{
+			
+		}
+		catch(Exception e){
+		}
+		}
 		// TODO Auto-generated method stub
 
 	}
 
-}
+

894ee63c9e5a2a7131fd1535567263ab6a116007
kotisadineni <sadineni2107@gmail.com>
2017-05-15 10:27:02 +0530

    small changes in code

--- ExceptionDemos/src/com/exceptions/InhertianceDemo/Child.java
+++ ExceptionDemos/src/com/exceptions/InhertianceDemo/Child.java
@@ -2,12 +2,18 @@ package com.exceptions.InhertianceDemo;
 
 public class Child extends Parent {
 	
-	public void m1(){
+	
+	
+	public void  m1(){
 		
-		Parent p1 = new Child();
-		p1.m1();
-		System.out.println("From Child");
+	System.out.println("From Child");
 		
 	}
 
+	
+	public static void main(String[] args) {
+	
+		Parent p1 = new Child();
+		p1.m1();
+}
 }
